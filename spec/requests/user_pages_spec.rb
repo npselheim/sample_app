bde831eadf4d888243845314eceb5a819120e4c2
Norm Selheim <norman.selheim@thomsonreuters.com>
2013-01-15 15:31:13 -0500

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,20 +4,62 @@ describe "User pages" do
 	
 	subject { page }
 
+	describe "index" do
+
+		let(:user) { FactoryGirl.create(:user) }
+
+		before do
+			sign_in user
+			visit users_path
+		end
+
+		it { should have_title('All users') }
+		it { should have_header('All users') }
+
+		describe "pagination" do
+			
+#			it { should have_selector('div.pagination') }
+
+			it "should list each user" do
+				User.paginate(page: 1).each do |user|
+					page.should have_selector('li', text: user.name)
+				end
+			end
+		end
+
+		describe "delete links" do
+			it { should_not have_link('delete') }
+
+			describe "as an admin user" do
+				let(:admin) { FactoryGirl.create(:admin) }
+				before do
+					sign_in admin
+					visit users_path
+				end
+
+				it { should have_link('delete', href: user_path(User.first)) }
+				it "should be able to delete another user" do
+					expect { click_link('delete') }.to change(User, :count).by(-1)
+				end
+				it { should_not have_link('delete', href: user_path(admin)) }
+			end
+		end
+	end
+
 	describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
 
-		it { should have_selector('h1',    text: user.name) }
-		it { should have_selector('title', text: user.name) }
+		it { should have_header(user.name) }
+		it { should have_title(user.name) }
 	end
 
 	describe "signup page" do
 
 		before { visit signup_path }
 
-		it { should have_selector('h1',    text: 'Sign up') }
-		it { should have_selector('title', text: 'Sign up') }
+		it { should have_header('Sign up') }
+		it { should have_title('Sign up') }
 	end
 
 	describe "signup" do
@@ -34,7 +76,7 @@ describe "User pages" do
 
 				before { click_button submit }
 
-				it { should have_selector('title', text: 'Sign up') }
+				it { should have_title('Sign up') }
 				it { should have_content('error') }
 				it { should have_selector('li', text: "Password can't be blank") }
 				# it { should_not have_selector('li', text: "digest") }
@@ -63,10 +105,49 @@ describe "User pages" do
 				before { click_button submit }
 				let(:user) { User.find_by_email('user@example.com') }
 
-				it { should have_selector('title', text: user.name) }
-				it { should have_selector('div.alert.alert-success', text: 'Welcome')}
+				it { should have_title(user.name) }
+				it { should have_success_message('Welcome') }
 				it { should have_link('Sign out') }
 			end
 		end
 	end
+
+	describe "edit" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in user
+		 	visit edit_user_path(user) 
+		end
+
+		describe "page" do
+			it { should have_header("Update your profile") }
+			it { should have_title("Edit user") }
+			it { should have_link('change', href: 'http://gravatar.com/emails') }
+		end
+
+		describe "with invalid information" do
+			before { click_button "Save changes" }
+
+			it { should have_content('error') }
+		end
+
+		describe "with valid information" do
+			let(:new_name)  { "New Name"}
+			let(:new_email) { "new@example.com" }
+			before do
+				fill_in "Name",             with: new_name
+				fill_in "Email",            with: new_email
+				fill_in "Password",         with: user.password
+				fill_in "Confirm Password", with: user.password
+				click_button "Save changes"
+			end
+
+			it { should have_title(new_name) }
+			it { should have_success_message('') }
+			it { should have_link('Sign out', href: signout_path) }
+			specify { user.reload.name.should  == new_name }
+			specify { user.reload.email.should == new_email }
+		end
+	end
+
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -20,3 +20,31 @@ RSpec::Matchers.define :have_error_message do |message|
 		page.should have_selector('div.alert.alert-error', text: message)
 	end
 end
+
+RSpec::Matchers.define :have_success_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-success', text: message)
+	end
+end
+
+RSpec::Matchers.define :have_header do |text|
+	match do |page|
+		page.should have_selector('h1',    text: text)
+	end
+end
+
+RSpec::Matchers.define :have_title do |text|
+	match do |page|
+		page.should have_selector('title', text: text)
+	end
+end
+
+def sign_in(user)
+	visit signin_path
+	fill_in "Email",    with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token 
+end
+
